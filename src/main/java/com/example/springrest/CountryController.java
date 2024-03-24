c18c3bb7e196bf4c0b2d893bb260765a6c797bfa
mikeqiu <mikeqiu2011@163.com>
2024-03-24 11:05:07 +0800

    return list of objects

--- src/main/java/com/example/springrest/CountryController.java
+++ src/main/java/com/example/springrest/CountryController.java
@@ -3,11 +3,20 @@ package com.example.springrest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class CountryController {
     @GetMapping("/france")
     public Country france(){
         var france = new Country("france", 67);
         return france;
+    }
+
+    @GetMapping("/all")
+    public List<Country> all(){
+        var france = new Country("france", 67);
+        var spain = new Country("spain", 47);
+        return List.of(france, spain);
     }
 }
